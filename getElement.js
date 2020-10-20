d5222f4e34427f7668b84d84b0ebfa0f4f3b96af
Yeri <yerikim1028@gmail.com>
2020-10-21 00:55:27 +0900

    Create getElement.js

---
+++ getElement.js
@@ -0,0 +1,19 @@
+// 강의 내용 코드를 테스트 하세요.
+
+
+
+// Assignment1 - 다음 함수 안에 코드를 구현하세요
+
+function getElement() {
+  let arr = [3, [4, ["array", 9], 2+3], [0]];
+ 
+}
+
+
+// Assignment2 - 다음 함수 안에 코드를 구현하세요
+// ex) myArray = [3, 2, 5];
+function addFirstAndLast(myArray) {
+ console.log("replit")
+}
+
+addFirstAndLast();
